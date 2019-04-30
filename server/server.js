4308fd68df426bb9c7e77f4018adf7b0867ecf77
Carlos Benavides <carlos.benavides@digitas.com>
2019-04-29 21:43:58 -0600

    hardcoding api routes to local 3001 for testing + adding server.js to client app

--- server/server.js
+++ server/server.js
@@ -8,6 +8,12 @@ const apiRoutes = require('./routes/apiRoutes');
 
 const API_PORT = 3001;
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
+
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
